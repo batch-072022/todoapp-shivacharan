72aaf7a0c1afb26cb61a08fd110c365174d7b702
Shiva Charan Brahmandlapally <shivacharan.brahmandlapally@valuemomentum.com>
2022-08-02 17:45:52 +0530

    comitting workspace

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { ListtaskComponent } from './listtask/listtask.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ViewUserComponent } from './view-user/view-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'listtask',component:ListtaskComponent},
+  {path:'edit-task',component:EditTaskComponent},
+  {path:'addtask',component:AddTaskComponent},
+  {path:'viewuser',component:ViewUserComponent},
+  {path:'users',component:ListUsersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { HeaderComponent } from './header/header.component';
+import { ListtaskComponent } from './listtask/listtask.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { ViewUserComponent } from './view-user/view-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    AddTaskComponent,
+    EditTaskComponent,
+    HeaderComponent,
+    ListtaskComponent,
+    ListUsersComponent,
+    ViewUserComponent
   ],
   imports: [
     BrowserModule,
